5011e9ee188e345a89c4aaeb073bb544d83dd880
idttm <andrew79911997@gmail.com>
2025-07-26 17:30:54 -0500

    Initial Commit

---
+++ FitnessShowcase Watch App/FitnessShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessShowcaseApp.swift
+//  FitnessShowcase Watch App
+//
+//  Created by Andrew Cheberyako on 26.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct FitnessShowcase_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessShowcase/FitnessShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessShowcaseApp.swift
+//  FitnessShowcase
+//
+//  Created by Andrew Cheberyako on 26.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct FitnessShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
